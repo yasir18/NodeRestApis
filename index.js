091666af9ab0892e40181ebc767f1eca7e7caffe
AFFAN <ya185024@ncr.com>
2019-06-21 00:31:54 +0530

    module.exports

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
 
 
-var http=require('http');
-var fs=require('fs');
+// var http=require('http');
+// var fs=require('fs');
 
-function handle(req,res){
-    switch(req.url){
-        case '/':
-            fs.readFile("index.html",function(err,contents){
-                res.write(contents);
-                res.end();
-            });          
-            break;
-        case '/about':
-            res.write('About page');
-            res.end();
-            break;
-        case '/contact':
-            res.write('Contact page');
-            res.end();
-            break;
-        case '/products':
-                var products=[
-                    {id:1,brand:"apple",price:400},
-                    {id:2,brand:"samsung",price:300},
-                    {id:3,brand:"Xiaomi",price:200}
-                ];
-                //write function will take only string
-                //so convert json to string
-                res.write(JSON.stringify(products));
-                res.end();
-                break;    
-        default:
-            res.write('Home Page');
-            break;
-            res.end();
-    }
+// function handle(req,res){
+//     switch(req.url){
+//         case '/':
+//             fs.readFile("index.html",function(err,contents){
+//                 res.write(contents);
+//                 res.end();
+//             });          
+//             break;
+//         case '/about':
+//             res.write('About page');
+//             res.end();
+//             break;
+//         case '/contact':
+//             res.write('Contact page');
+//             res.end();
+//             break;
+//         case '/products':
+//                 var products=[
+//                     {id:1,brand:"apple",price:400},
+//                     {id:2,brand:"samsung",price:300},
+//                     {id:3,brand:"Xiaomi",price:200}
+//                 ];
+//                 //write function will take only string
+//                 //so convert json to string
+//                 res.write(JSON.stringify(products));
+//                 res.end();
+//                 break;    
+//         default:
+//             res.write('Home Page');
+//             break;
+//             res.end();
+//     }
     
-}
+// }
 
-var server=http.createServer(handle);
-server.listen(3000);
-console.log("server running on port 3000");
+// var server=http.createServer(handle);
+// server.listen(3000);
+// console.log("server running on port 3000");
+
+
+
+var calculator=require('./math');
+console.log(calculator.display2());
+console.log(calculator.myProperty2);
