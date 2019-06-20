b27da072ae4306c04a84113a259262ed57684afc
AFFAN <ya185024@ncr.com>
2019-06-21 00:46:38 +0530

    basic express server

--- index.js
+++ index.js
@@ -1,49 +1,17 @@
+var express=require('express');
+var app=express();
+app.listen(3000,function(){
+    console.log("server is running on port 3000");
+});
 
 
-// var http=require('http');
-// var fs=require('fs');
+//routing HttpGet
+app.get('/',function(req,res){
+    res.send("hello express");
+});
 
-// function handle(req,res){
-//     switch(req.url){
-//         case '/':
-//             fs.readFile("index.html",function(err,contents){
-//                 res.write(contents);
-//                 res.end();
-//             });          
-//             break;
-//         case '/about':
-//             res.write('About page');
-//             res.end();
-//             break;
-//         case '/contact':
-//             res.write('Contact page');
-//             res.end();
-//             break;
-//         case '/products':
-//                 var products=[
-//                     {id:1,brand:"apple",price:400},
-//                     {id:2,brand:"samsung",price:300},
-//                     {id:3,brand:"Xiaomi",price:200}
-//                 ];
-//                 //write function will take only string
-//                 //so convert json to string
-//                 res.write(JSON.stringify(products));
-//                 res.end();
-//                 break;    
-//         default:
-//             res.write('Home Page');
-//             break;
-//             res.end();
-//     }
-    
-// }
+app.get('/health',function(req,res){
+    var response={status:'up'};
+    res.json(response);
+});
 
-// var server=http.createServer(handle);
-// server.listen(3000);
-// console.log("server running on port 3000");
-
-
-
-var calculator=require('./math');
-console.log(calculator.display2());
-console.log(calculator.myProperty2);
